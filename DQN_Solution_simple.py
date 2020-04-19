d22b966a30a431d1c7b1df02b053b634f0e9f07c
Susanna Maria <susanna@olsoni.de>
2020-04-19 10:21:49 +0200

    Rainbow adapted for banana

--- DQN_Solution_simple.py
+++ DQN_Solution_simple.py
@@ -48,9 +48,10 @@ print('States look like:', state)
 state_size = len(state)
 print('States have length:', state_size)
 
+
 agent = Agent(state_size=state_size, action_size=action_size, seed=0)
 
-def dqn(n_episodes=20000, max_t=1000, eps_start=1, eps_end=0.001, eps_decay=0.995):
+def dqn(n_episodes=20000, max_t=10000, eps_start=0.1, eps_end=0.001, eps_decay=0.9995):
     """Deep Q-Learning.
     
     Params
@@ -63,9 +64,7 @@ def dqn(n_episodes=20000, max_t=1000, eps_start=1, eps_end=0.001, eps_decay=0.99
     """
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-
-    eps = eps_start 
-                       # initialize epsilon
+    eps = eps_start                    # initialize epsilon
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name] # reset the environment
         state = env_info.vector_observations[0]            # get the current state
@@ -80,8 +79,6 @@ def dqn(n_episodes=20000, max_t=1000, eps_start=1, eps_end=0.001, eps_decay=0.99
             next_state = env_info.vector_observations[0]   # get the next state
             reward = env_info.rewards[0]                   # get the reward
             done = env_info.local_done[0]                  # see if episode has finished
-            agent.step(state,action,reward,next_state,done)
-           
             score += reward                                # update the score
             state = next_state                             # roll over the state to next time step
             if done:                                       # exit loop if episode finished
@@ -97,7 +94,7 @@ def dqn(n_episodes=20000, max_t=1000, eps_start=1, eps_end=0.001, eps_decay=0.99
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             break
-    
+    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_2000.pth')
     return scores
 
 scores = dqn()
